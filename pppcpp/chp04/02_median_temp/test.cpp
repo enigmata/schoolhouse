3b8c8775c0d2a9cb714c8abb6b9d4162d6d0ecc8
Randy Horman <rwhorman@gmail.com>
2017-07-15 23:05:11 -0400

    some basic unit tests for median temp

---
+++ pppcpp/chp04/02_median_temp/test.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+#include "sol.hpp"
+
+std::vector<double> t0 = {};
+std::vector<double> t1 = {23.5};
+std::vector<double> t2 = {23.2,33.3};
+std::vector<double> t3 = {33.3,23.2,44.4};
+
+TEST_CASE("Simple test of basic variants", "[median]")
+{
+    REQUIRE(median_temp(t0)==0.0);
+    REQUIRE(median_temp(t1)==23.5);
+    REQUIRE(median_temp(t2)==28.25);
+    REQUIRE(median_temp(t3)==33.3);
+}
