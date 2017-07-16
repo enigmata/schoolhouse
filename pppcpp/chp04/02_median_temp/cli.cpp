e0a4b5446a8f83611328344c0f4bb04839596379
Randy Horman <rwhorman@gmail.com>
2017-07-15 22:59:47 -0400

    a cli to test the median temp solution

---
+++ pppcpp/chp04/02_median_temp/cli.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include "sol.hpp"
+
+int main()
+{
+    std::cout << "\nExercise: Print the median temperature, a number so that\n"
+              << "exactly as many elements come before it in the sequence\n"
+              << "as come after it.\n\n"
+              << "Enter one or more temperatures, then enter Ctrl-D to exit:\n";
+
+    std::vector<double> temps;
+
+    for (double temp; std::cin >> temp;)
+        temps.push_back(temp);
+
+    if (temps.empty())
+    {
+        std::cout << "Error: No temperatures were entered\n";
+        return 1;
+    }
+
+    std::cout << "The median temperature is " << median_temp(temps) << "\n";
+    
+    return 0;
+}
