3bf49473e6cc937ccf1c5a480ff8aaebd7605ab5
Randy Horman <rwhorman@gmail.com>
2017-07-22 09:40:44 -0400

    grudingly accepting clang-format's default style of 2-char-space tabs

--- pppcpp/chp04/02_median_temp/median_temp.cpp
+++ pppcpp/chp04/02_median_temp/median_temp.cpp
@@ -3,23 +3,22 @@
 #include <vector>
 
 double median_temp(std::vector<double> &temps) {
-    double median = 0.0;
+  double median = 0.0;
 
-    if (temps.empty()) {
-        throw std::invalid_argument("At least one temperature is required");
+  if (temps.empty()) {
+    throw std::invalid_argument("At least one temperature is required");
+  } else {
+    std::sort(temps.begin(), temps.end());
+
+    if (temps.size() % 2 == 0) {
+      // The median of an even number of temperatures is the
+      // mean of middle two temperatures in the sequence.
+      int middle_left = (temps.size() / 2) - 1, middle_right = middle_left + 1;
+      median = (temps[middle_left] + temps[middle_right]) / 2.0;
     } else {
-        std::sort(temps.begin(), temps.end());
-
-        if (temps.size() % 2 == 0) {
-            // The median of an even number of temperatures is the 
-            // mean of middle two temperatures in the sequence.
-            int middle_left = (temps.size() / 2) - 1,
-                middle_right = middle_left + 1;
-            median = (temps[middle_left] + temps[middle_right]) / 2.0;
-        } else {
-            median = temps[temps.size() / 2]; // depend on rounding down
-        }
+      median = temps[temps.size() / 2]; // depend on rounding down
     }
+  }
 
-    return median;
+  return median;
 }
