51ce229ec8c9c03ee257de885320054681127dc8
Randy Horman <rwhorman@gmail.com>
2020-09-01 20:59:31 -0400

    import my solution to the fifth problem

---
+++ euler/p0005/sol2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main( int argc, char *argv[] )
+{
+    // multiple all prime factors of 1 .. 20
+    //  2, 3, 2, 5, 7, 2, 3, 11, 13, 2, 17, 19
+    //  yields 232792560
+
+    return( 0 );
+}
